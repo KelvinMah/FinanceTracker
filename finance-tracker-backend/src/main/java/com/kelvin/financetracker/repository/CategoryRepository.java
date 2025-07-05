e02bd8d4462239df9f0745ddc4032ccf1b456e00
KelvinMah <113228318+KelvinMah@users.noreply.github.com>
2025-07-05 20:48:15 +0800

    Delete finance-tracker-backend/src directory

--- finance-tracker-backend/src/main/java/com/kelvin/financetracker/repository/CategoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.kelvin.financetracker.repository;
-
-import com.kelvin.financetracker.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {}
--- finance-tracker-backend/src/main/java/com/kelvin/financetracker/repository/TransactionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kelvin.financetracker.repository;
-
-import com.kelvin.financetracker.model.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-    List<Transaction> findByCategoryName(String categoryName);
-}
